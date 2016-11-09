c95e0744a5767d2ab8c30736846b14d49f3d5f7d
hp <hui.pheng.teh@intel.com>
2016-11-10 07:15:07 +0800

    my first unit test

--- mytest_test.c
+++ mytest_test.c
@@ -32,46 +32,59 @@ ssize_t __wrap_write(int fildes, const void *buf, size_t nbytes)
     return (ssize_t)mock();
 }
 
-static void test_readwrite(void **state)
+static void test_readwrite_success(void **state)
 {
     char *ret;   
     (void)state;
     will_return(__wrap_read, 0);
-    will_return(__wrap_read, 10);
- 
     will_return(__wrap_write, 0);
-    will_return (__wrap_write, 9);
 
     ret = printout("");
     assert_string_equal(ret, "success");
-
-    ret = printout("hotdog1234");
-    assert_string_equal(ret, "error2");
-
 }
 
-static void test_readwrite2(void **state)
+static void test_readwrite_error1(void **state)
 {
     char *ret;
     (void)state;
     will_return(__wrap_read, 4);
-    will_return(__wrap_read, 7);
-
     will_return(__wrap_write, 4);
-    will_return (__wrap_write, 6);
 
     ret = printout("hot");
     assert_string_equal(ret, "error1");
+}
+
+static void test_readwrite_error2(void **state)
+{
+    char *ret;
+    (void)state;
+    will_return(__wrap_read, 10);
+    will_return (__wrap_write, 9);
+
+    ret = printout("hotdog1234");
+    assert_string_equal(ret, "error2");
+}
+
+static void test_readwrite_error3(void **state)
+{
+    char *ret;
+    (void)state;
+    will_return(__wrap_read, 7);
+    will_return (__wrap_write, 6);
+
     ret = printout("hotdog");
     assert_string_equal(ret, "error3");
 }
 
 
+
 int main(void)
 {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_readwrite),
-        cmocka_unit_test(test_readwrite2),
+        cmocka_unit_test(test_readwrite_success),
+        cmocka_unit_test(test_readwrite_error1),
+	cmocka_unit_test(test_readwrite_error2),
+	cmocka_unit_test(test_readwrite_error3),
    }; 
  return cmocka_run_group_tests(tests, NULL, NULL);
 
