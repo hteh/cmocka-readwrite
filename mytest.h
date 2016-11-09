37c29147a753e79a3b80eec149da1264e40c18a2
hp <hui.pheng.teh@intel.com>
2016-11-10 07:18:36 +0800

    my first unit test

--- mytest.h
+++ mytest.h
@@ -1,12 +1,9 @@
-//#ifndef IO_TOOLS_H
-//#define IO_TOOLS_H
 
 #include <unistd.h>
 
 char *printout(char *data);
-const char* errormsg(int error);
 
 extern ssize_t read(int filedes, void *buf, size_t nbytes);
 extern ssize_t write(int fildes, const void *buf, size_t nbytes);
 
-//#endif
+
